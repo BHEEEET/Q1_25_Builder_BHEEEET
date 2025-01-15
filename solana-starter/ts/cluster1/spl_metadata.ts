d4438f36586477f5b05e72ecd720c8e0e8a57f09
krkbheet@gmail.com <krkbheet@gmail.com>
2025-01-15 23:21:47 +0100

    class 2: createMetadataAccount

--- solana-starter/ts/cluster1/spl_metadata.ts
+++ solana-starter/ts/cluster1/spl_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../dev-wallet.json"
+import wallet from "../../../0-pre-req/rust/Turbine-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { 
     createMetadataAccountV3, 
@@ -10,7 +10,7 @@ import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-fo
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("4NJp9AzfaTkdZCD2DHpouqpg4nTn6QZBtLTQURmyK9SM")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -21,28 +21,38 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint,
+            mintAuthority: signer
+        }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: "BHEETTurbin3",
+            symbol: "BT3",
+            uri: "",
+            sellerFeeBasisPoints: 1000, // 1000 = 10% fee
+            creators: null,
+            collection: null,
+            uses: null
+        }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data,
+            isMutable: true,
+            collectionDetails: null
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
+        let result = await tx.sendAndConfirm(umi);
+        console.log(bs58.encode(result.signature));
+        // 3zZviR63RC7Ys9qb43zoVD1EGhoN7JmNW7RFc4htUcubsVk5E1opHURcWhsecjrrdnLbeiK9f7sQvptoUXNHqtUU
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
