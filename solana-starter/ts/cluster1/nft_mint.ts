75e8a8d81342a261f1f4d5ddca3efe3c0d7d77bb
krkbheet@gmail.com <krkbheet@gmail.com>
2025-01-16 23:35:05 +0100

    class 3: mint NFT using createNft() and by passing in the metadata uri

--- solana-starter/ts/cluster1/nft_mint.ts
+++ solana-starter/ts/cluster1/nft_mint.ts
@@ -2,7 +2,7 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../wba-wallet.json"
+import wallet from "../../../0-pre-req/rust/Turbine-wallet.json"
 import base58 from "bs58";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
@@ -16,11 +16,19 @@ umi.use(mplTokenMetadata())
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
+    let tx = createNft(umi, {
+        mint: mint,
+        name: "Dean's List v2 Devnet #2",
+        symbol: "DL v2",
+        // pass metadata uri
+        uri: "https://devnet.irys.xyz/C445XKtAx5kUgRPmpjzToMKhT8yCfub2pfKmo9Ru21mb",
+        sellerFeeBasisPoints: percentAmount(5),
+    })
+    let result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
     
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+    // https://explorer.solana.com/tx/4jE8cBCogbpFCQXNCrf5dBB5EyZgSm7px1ZwSbT1c9R195gCpCC6UYVTURsF2V5NgapzjCvqva33ZKJzFJTXDNSj?cluster=devnet
 
     console.log("Mint Address: ", mint.publicKey);
 })();
